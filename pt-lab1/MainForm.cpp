f4afb25ca29335a1d019ce5f8a6f2be8e8425ea3
Konstantin <dr.kon.me3@gmail.com>
2018-03-30 20:50:03 +0300

    changed structure

--- pt-lab1/MainForm.cpp
+++ pt-lab1/MainForm.cpp
@@ -13,3 +13,170 @@ int main(array<System::String ^> ^args)
 	ptlab1::MainForm form;   
 	Application::Run(%form);
 }
+
+System::Void ptlab1::MainForm::mainButton_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	double n;
+	bool flag = false;
+	int nExperiments = System::Convert::ToInt32(textBox->Text);
+	int i = 0;
+	int result;
+
+	for (int j = 0; j < nExperiments; j++)
+	{
+		experiment->SetAmountOfLightbulbs(System::Convert::ToInt32(N_textBox->Text));
+		experiment->SetAmountOfSelected(System::Convert::ToInt32(R_textBox->Text));
+		experiment->SetAmountOfDamaged(System::Convert::ToInt32(M_textBox->Text));
+
+		n = 0;
+		flag = false;
+		result = experiment->RunExperiment();
+		expandedResults->push_back(result); // запоминаем результат
+		count++;
+
+		// ищем результат в таблице
+		for (i = 0; i < rows; i++)
+		{
+			if (result == System::Convert::ToInt32(dataGridView->Rows[i]->Cells[0]->Value)) {
+				n = System::Convert::ToInt32(dataGridView->Rows[i]->Cells[1]->Value) + 1;
+				dataGridView->Rows[i]->Cells[1]->Value = n;
+				flag = true;
+				break;
+			}
+		}
+
+		// если не нашли, то добавляем в таблицу
+		if (flag == false) {
+			results->push_back(result); // также добавляем в массив результатов 
+			std::sort(results->begin(), results->end()); // и сортируем 
+			dataGridView->Rows->Add();
+			dataGridView->Rows[rows]->Cells[0]->Value = result;
+			dataGridView->Rows[rows]->Cells[1]->Value = 1;
+
+			// пересоздаем условия эксперимента, чтобы посчитать теоретическую вероятность
+			experiment->SetAmountOfLightbulbs(System::Convert::ToInt32(N_textBox->Text));
+			experiment->SetAmountOfSelected(System::Convert::ToInt32(R_textBox->Text));
+			experiment->SetAmountOfDamaged(System::Convert::ToInt32(M_textBox->Text));
+			dataGridView->Rows[rows]->Cells[3]->Value = experiment->GetProbability(result);
+
+			// сортировка таблицы по возрастанию
+			dataGridView->Sort(column1, System::ComponentModel::ListSortDirection::Ascending);
+			rows++;
+		}
+
+		frequencies->clear();
+		// обновление частот с учетом нового эксперимента
+		for (i = 0; i < rows; i++)
+		{
+			n = System::Convert::ToDouble(dataGridView->Rows[i]->Cells[1]->Value);
+			dataGridView->Rows[i]->Cells[2]->Value = n / count;
+
+			// также добавляем в массив частот
+			frequencies->push_back(System::Convert::ToDouble(dataGridView->Rows[i]->Cells[2]->Value));
+		}
+	}
+
+	dataGridView->ClearSelection();
+
+	// сортируем все полученные значения (для вычисления медианы и размаха выборки)
+	std::sort(expandedResults->begin(), expandedResults->end());
+
+	// также требуется рассчитать характеристики
+	CalculateCharacteristics();
+}
+
+System::Void ptlab1::MainForm::delete_button_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	dataGridView->Rows->Clear();
+	dataGridView_table->Rows->Clear();
+	frequencies->clear();
+	results->clear();
+	expandedResults->clear();
+	maxError_textBox->Text = "";
+	count = 0;
+	rows = 0;
+}
+
+double ptlab1::MainForm::GetExpectedValue()
+{
+	double sum = 0.0;
+
+	for (int i = 0; i < results->size(); i++)
+		sum += results->at(i) * frequencies->at(i);
+
+	return sum;
+}
+
+double ptlab1::MainForm::GetDispersion()
+{
+	double EV = GetExpectedValue();
+	double sum = 0.0;
+
+	for (int i = 0; i < results->size(); i++)
+		sum += pow((results->at(i) - EV), 2) * frequencies->at(i);
+
+	return sum;
+}
+
+double ptlab1::MainForm::GetMedian()
+{
+	double median = 0.0;
+	int size = (int)expandedResults->size();
+
+	if (size == 1) median = expandedResults->at(size - 1);
+	else
+	{
+		if (size % 2 == 0)
+			median = (double)(expandedResults->at(size / 2 - 1) + expandedResults->at(size / 2)) / 2;
+		else
+			median = expandedResults->at(size / 2);
+	}
+
+	return median;
+}
+
+double ptlab1::MainForm::GetMaxError()
+{
+	double maxError = 0.0;
+	double diff = 0.0;
+	double a = 0.0;
+	double b = 0.0;
+
+	for (int i = 0; i < rows; i++) {
+		a = System::Convert::ToDouble(dataGridView->Rows[i]->Cells[2]->Value);
+		b = System::Convert::ToDouble(dataGridView->Rows[i]->Cells[3]->Value);
+		diff = fabs(a - b);
+		if (maxError < diff) maxError = diff;
+	}
+
+	return maxError;
+}
+
+void ptlab1::MainForm::CalculateCharacteristics()
+{
+	// пересоздаем условия эксперимента
+	experiment->SetAmountOfLightbulbs(System::Convert::ToInt32(N_textBox->Text));
+	experiment->SetAmountOfSelected(System::Convert::ToInt32(R_textBox->Text));
+	experiment->SetAmountOfDamaged(System::Convert::ToInt32(M_textBox->Text));
+
+	double theoryEV = experiment->GetExpectedValue();
+	double realEV = GetExpectedValue();
+	double theoryD = experiment->GetDispersion();
+	double realD = GetDispersion();
+	double median = GetMedian();
+	int range = expandedResults->back() - expandedResults->front();
+	double maxError = GetMaxError();
+
+	dataGridView_table->Rows->Clear();
+	dataGridView_table->Rows[0]->Cells[0]->Value = theoryEV; // теоретическое мат. ожидание
+	dataGridView_table->Rows[0]->Cells[1]->Value = realEV;	 // выборочное мат. ожидание
+	dataGridView_table->Rows[0]->Cells[2]->Value = fabs(theoryEV - realEV);	// отклонение мат. ожидания от теоретического
+	dataGridView_table->Rows[0]->Cells[3]->Value = theoryD;  // теоретическая дисперсия
+	dataGridView_table->Rows[0]->Cells[4]->Value = realD;    // выборочная дисперсия
+	dataGridView_table->Rows[0]->Cells[5]->Value = fabs(theoryD - realD);   // отклонение дисперсии
+	dataGridView_table->Rows[0]->Cells[6]->Value = median;   // выборочная медиана
+	dataGridView_table->Rows[0]->Cells[7]->Value = range;	 // размах выборки
+	dataGridView_table->ClearSelection();
+
+	maxError_textBox->Text = System::Convert::ToString(maxError); // максимальное отклонение частоты от вероятности
+}
--- pt-lab1/MainForm.h
+++ pt-lab1/MainForm.h
@@ -16,6 +16,7 @@ namespace ptlab1 {
 	/// </summary>
 	public ref class MainForm : public System::Windows::Forms::Form
 	{	
+	protected:
 		Experiment* experiment;
 		std::vector<int> *results;		   // все полученные значения СВ
 		std::vector<int> *expandedResults; // все полученные значения СВ (с повторами)
@@ -58,12 +59,10 @@ namespace ptlab1 {
 			results = new std::vector<int>;
 			expandedResults = new std::vector<int>;
 			frequencies = new std::vector<double>;		
-			count = 0; rows = 0;
+			count = 0; 
+			rows = 0;
 
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
 		}
 
 	protected:
@@ -436,170 +435,17 @@ namespace ptlab1 {
 
 		}
 #pragma endregion
-	private: System::Void mainButton_Click(System::Object^  sender, System::EventArgs^  e) {
-		double n;
-		bool flag = false;
-		int nExperiments = System::Convert::ToInt32(textBox->Text);
-		int i = 0;
-		int result;
 
-		for (int j = 0; j < nExperiments; j++)
-		{
-			experiment->SetAmountOfLightbulbs(System::Convert::ToInt32(N_textBox->Text));
-			experiment->SetAmountOfSelected(System::Convert::ToInt32(R_textBox->Text));
-			experiment->SetAmountOfDamaged(System::Convert::ToInt32(M_textBox->Text));		
+	private: System::Void mainButton_Click(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void delete_button_Click(System::Object^  sender, System::EventArgs^  e);
 
-			n = 0;
-			flag = false;
-			result = experiment->RunExperiment();
-			expandedResults->push_back(result); // запоминаем результат
-			count++;
-
-			// ищем результат в таблице
-			for (i = 0; i < rows; i++)
-			{
-				if (result == System::Convert::ToInt32(dataGridView->Rows[i]->Cells[0]->Value)) {
-					n = System::Convert::ToInt32(dataGridView->Rows[i]->Cells[1]->Value) + 1;
-					dataGridView->Rows[i]->Cells[1]->Value = n;
-					flag = true;
-					break;
-				}
-			}
-
-			// если не нашли, то добавляем в таблицу
-			if (flag == false) {
-				results->push_back(result); // также добавляем в массив результатов 
-				std::sort(results->begin(), results->end()); // и сортируем 
-				dataGridView->Rows->Add();			
-				dataGridView->Rows[rows]->Cells[0]->Value = result;
-				dataGridView->Rows[rows]->Cells[1]->Value = 1;
-				
-				// пересоздаем условия эксперимента, чтобы посчитать теоретическую вероятность
-				experiment->SetAmountOfLightbulbs(System::Convert::ToInt32(N_textBox->Text));
-				experiment->SetAmountOfSelected(System::Convert::ToInt32(R_textBox->Text));
-				experiment->SetAmountOfDamaged(System::Convert::ToInt32(M_textBox->Text));
-				dataGridView->Rows[rows]->Cells[3]->Value = experiment->GetProbability(result);
-
-				// сортировка таблицы по возрастанию
-				dataGridView->Sort(column1, System::ComponentModel::ListSortDirection::Ascending);
-				rows++;
-			}
-
-			frequencies->clear();
-			// обновление частот с учетом нового эксперимента
-			for (i = 0; i < rows; i++)
-			{
-				n = System::Convert::ToDouble(dataGridView->Rows[i]->Cells[1]->Value);
-				dataGridView->Rows[i]->Cells[2]->Value = n / count;
-
-				// также добавляем в массив частот
-				frequencies->push_back(System::Convert::ToDouble(dataGridView->Rows[i]->Cells[2]->Value));
-			}
-		}
-
-		dataGridView->ClearSelection();
-
-		// сортируем все полученные значения (для вычисления медианы и размаха выборки)
-		std::sort(expandedResults->begin(), expandedResults->end()); 
-
-		// также требуется рассчитать характеристики
-		CalculateCharacteristics();	
-	}
-
-	private: System::Void delete_button_Click(System::Object^  sender, System::EventArgs^  e) {	
-		dataGridView->Rows->Clear();
-		dataGridView_table->Rows->Clear();
-		frequencies->clear();
-		results->clear();	
-		expandedResults->clear();
-		maxError_textBox->Text = "";
-		count = 0;
-		rows = 0;
-	}
+	protected: 
+		double GetExpectedValue(); // математическое ожидание по проведенным экспериментам
+		double GetDispersion();	   // дисперсия по проведенным экспериментам
+		double GetMedian(); 	   // выборочная медиана
+		double GetMaxError();	   // максимальное отклонение частот от вероятностей
 
 	// расчет основных характеристик на основе проведенных экспериментов
-	protected: void CalculateCharacteristics() {
-		// пересоздаем условия эксперимента
-		experiment->SetAmountOfLightbulbs(System::Convert::ToInt32(N_textBox->Text));
-		experiment->SetAmountOfSelected(System::Convert::ToInt32(R_textBox->Text));
-		experiment->SetAmountOfDamaged(System::Convert::ToInt32(M_textBox->Text));
-
-		double theoryEV = experiment->GetExpectedValue();
-		double realEV = GetExpectedValue();
-		double theoryD = experiment->GetDispersion();
-		double realD = GetDispersion();
-		double median = GetMedian();
-		int range = expandedResults->back() - expandedResults->front();
-		double maxError = GetMaxError();
-
-		dataGridView_table->Rows->Clear();
-		dataGridView_table->Rows[0]->Cells[0]->Value = theoryEV; // теоретическое мат. ожидание
-		dataGridView_table->Rows[0]->Cells[1]->Value = realEV;	 // выборочное мат. ожидание
-		dataGridView_table->Rows[0]->Cells[2]->Value = fabs(theoryEV - realEV);	// отклонение мат. ожидания от теоретического
-		dataGridView_table->Rows[0]->Cells[3]->Value = theoryD;  // теоретическая дисперсия
-		dataGridView_table->Rows[0]->Cells[4]->Value = realD;    // выборочная дисперсия
-		dataGridView_table->Rows[0]->Cells[5]->Value = fabs(theoryD - realD);   // отклонение дисперсии
-		dataGridView_table->Rows[0]->Cells[6]->Value = median;   // выборочная медиана
-		dataGridView_table->Rows[0]->Cells[7]->Value = range;	 // размах выборки
-		dataGridView_table->ClearSelection();
-
-		maxError_textBox->Text = System::Convert::ToString(maxError); // максимальное отклонение частоты от вероятности
-	}
-
-	// математическое ожидание по проведенным экспериментам
-	protected: double GetExpectedValue() {
-		double sum = 0.0;
-
-		for (int i = 0; i < results->size(); i++)
-			sum += results->at(i) * frequencies->at(i);
-
-		return sum;
-	}
-	
-	// дисперсия по проведенным экспериментам
-	protected: double GetDispersion() {
-		double EV = GetExpectedValue();
-		double sum = 0.0;
-
-		for (int i = 0; i < results->size(); i++)
-			sum += pow((results->at(i) - EV), 2) * frequencies->at(i);
-
-		return sum;
-	}
-
-	// выборочная медиана
-	protected: double GetMedian() {
-		double median = 0.0;
-		int size = expandedResults->size();
-
-		if (size == 1) median = expandedResults->at(size - 1);
-		else
-		{
-			if (size % 2 == 0)
-				median = (double)(expandedResults->at(size/2 - 1) + expandedResults->at(size/2)) / 2;
-			else
-				median = expandedResults->at(size/2);		
-		}
-
-		return median;
-	}
-
-	// максимальное отклонение частот от вероятностей
-	protected: double GetMaxError() {
-		double maxError = 0.0;
-		double diff = 0.0;
-		double a = 0.0;
-		double b = 0.0;
-
-		for (int i = 0; i < rows; i++) {
-			a = System::Convert::ToDouble(dataGridView->Rows[i]->Cells[2]->Value);
-			b = System::Convert::ToDouble(dataGridView->Rows[i]->Cells[3]->Value);
-			diff = fabs(a - b);
-			if (maxError < diff) maxError = diff;
-		}
-
-		return maxError;
-	}
-
+		void CalculateCharacteristics();
 	};
 }
